1e1a409a16eac09d3f0f79af6f85e3cfcec99a44
lovejj1994 <455234037@qq.com>
2016-11-16 10:01:49 +0800

    2016.11.16 hadoop上传中

--- src/main/java/cn/xxywithpq/web/HadoopController.java
+++ src/main/java/cn/xxywithpq/web/HadoopController.java
@@ -10,8 +10,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.fs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.hadoop.store.DataStoreWriter;
-import org.springframework.data.hadoop.store.dataset.DatasetOperations;
 import org.springframework.data.hadoop.store.strategy.naming.ChainedFileNamingStrategy;
 import org.springframework.data.hadoop.store.strategy.naming.FileNamingStrategy;
 import org.springframework.data.hadoop.store.strategy.naming.RollingFileNamingStrategy;
@@ -23,37 +21,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import cn.xxywithpq.domain.Icon;
+import com.alibaba.fastjson.JSONObject;
+
 import cn.xxywithpq.hadoop.store.output.MyOutputStreamWriter;
 
 @RestController
 @RequestMapping("/hadoop/")
 public class HadoopController {
- 
+
 	private final static Log log = LogFactory.getLog(HadoopController.class);
-	
+
 	@Autowired
 	private org.apache.hadoop.conf.Configuration hadoopConfiguration;
 
-	private DataStoreWriter<Icon> writer;
-	private DatasetOperations datasetOperations;
-
-	@Autowired
-	public void setDatasetOperations(DatasetOperations datasetOperations) {
-		this.datasetOperations = datasetOperations;
-	}
-
-	@Autowired
-	public void setDataStoreWriter(DataStoreWriter<Icon> dataStoreWriter) {
-		this.writer = dataStoreWriter;
-	}
-
 	@RequestMapping(method = RequestMethod.POST, value = "icon/upload")
-	public void iconUpload(@RequestParam MultipartFile file, HttpServletRequest request, HttpServletResponse response)
+	public Object iconUpload(@RequestParam MultipartFile file, HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
 
-		try {
+		JSONObject object = new JSONObject();
 
+		try {
 			Path path = new Path("/icon3");
 			MyOutputStreamWriter writer = new MyOutputStreamWriter(hadoopConfiguration, path, null);
 
@@ -61,20 +48,23 @@ public class HadoopController {
 
 			UuidFileNamingStrategy uuidFileNamingStrategy = new UuidFileNamingStrategy();
 			StaticFileNamingStrategy staticFileNamingStrategy = new StaticFileNamingStrategy("icon");
-			StaticFileNamingStrategy staticFileNamingStrategy1 = new StaticFileNamingStrategy("png", ".");
+			StaticFileNamingStrategy staticFileNamingStrategyafter = new StaticFileNamingStrategy("png", ".");
 			RollingFileNamingStrategy rollingFileNamingStrategy = new RollingFileNamingStrategy();
+
 			strategy.setStrategies(Arrays.asList(new FileNamingStrategy[] { staticFileNamingStrategy,
-					uuidFileNamingStrategy, rollingFileNamingStrategy, staticFileNamingStrategy1 }));
+					uuidFileNamingStrategy, rollingFileNamingStrategy, staticFileNamingStrategyafter }));
+
 			writer.setFileNamingStrategy(strategy);
 			writer.write(file.getBytes());
 			Path filepath = writer.getFilePath();
-			log.info(filepath.toString());
+			String iconpath = "http://www.xxywithpq.cn:50070/webhdfs/v1" + filepath + "?op=OPEN";
+			log.info("return filepath = " + iconpath);
+			object.put("iconpath", iconpath);
 			writer.close();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		return object;
 	}
 
 }
