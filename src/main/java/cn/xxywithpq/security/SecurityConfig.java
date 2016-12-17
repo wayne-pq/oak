be57ca62741dcdc824d3f77cefaeaf18984bff8f
lovejj1994 <455234037@qq.com>
2016-12-17 18:24:35 +0800

    2016.12.17 完善注册机制

--- src/main/java/cn/xxywithpq/security/SecurityConfig.java
+++ src/main/java/cn/xxywithpq/security/SecurityConfig.java
@@ -76,63 +76,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-<<<<<<< HEAD
-		// http mapsto https
-		/*
-		 * .portMapper().http(8080).mapsTo(8444) .and()
-		 */
-		// super.configure(http);
-		// http.httpBasic();
-		http.cors().and().csrf().disable();
-		http
-		.authorizeRequests()
-			.anyRequest().authenticated()
-			.and()
-		.requestCache()
-			.requestCache(new NullRequestCache())
-			.and()
-		.httpBasic();
-		// http.csrf().disable()
-		// .formLogin()
-		// .loginPage("/login")
-		// .and()
-		// .authorizeRequests()
-		// .mvcMatchers(HttpMethod.GET,"/").authenticated()
-		// .anyRequest().permitAll()
-		// .and()
-//		http.authorizeRequests().antMatchers("/home/**").permitAll().antMatchers("/work/**").authenticated()
-//				.and().cors().configurationSource(corsConfigurationSource()).and().csrf().disable().httpBasic();
-		//http.formLogin().successForwardUrl("/home/loginsuccess").failureUrl("/home/loginfail");
-		// http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
-		/*
-		 * .requiresChannel() .antMatchers("/*") .requiresSecure() .and()
-		 */
-		// 谷歌游览器测试中，如果不关闭游览器，到了过期时间也不会重新登录，关闭后有效。
-		// .rememberMe().key("lovejj1994").tokenValiditySeconds(60)
-		// .and()
-		// .logout().logoutSuccessUrl("/").and()
-		// .sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry());
-		/*
-		 * .and() .requiresChannel() .antMatchers("/redirect*","/")
-		 * .requiresInsecure();
-		 */
-	}
-
-	// 解决跨域问题
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080","http://www.xxywithpq.cn"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
-		configuration.setAllowedHeaders(Arrays.asList("content-type","authorization","x-auth-token"));
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-=======
 		http.csrf().disable().cors();
 //		http.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll();
->>>>>>> panqian
 	}
 
 //	// 解决跨域问题
